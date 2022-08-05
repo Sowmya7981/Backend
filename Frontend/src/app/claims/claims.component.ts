29ff3808633cdda437c87393c22a6c4d41b35f81
Sowmya7981 <65537852+Sowmya7981@users.noreply.github.com>
2022-08-05 10:31:49 +0530

    Delete Frontend/src directory

--- Frontend/src/app/claims/claims.component.ts
+++
@@ -1,7 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-claims',
-  templateUrl: './claims.component.html',
-})
-export class ClaimsComponent {}
--- Frontend/src/environments/environment.prod.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-  production: true,
-  AUTH_SERVICE_URL: 'http://localhost:8008/authorization',
-  MEMBER_SERVICE_URL: 'http://localhost:8099/memberModule',
-  CLAIM_SERVICE_URL:"http://localhost:8010/claimModule"
-};
